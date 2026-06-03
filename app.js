4688d76075544380142943e4f9b96f65dfd253cd
aegontar <charlesdessa1@hotmail.com>
2026-06-03 16:54:59 +0600

    added an input so you can send data which will be saved in a text file

--- app.js
+++ app.js
@@ -1,7 +1,53 @@
 const http = require("http");
+const fs = require("fs");
+
+// 1. Catch the favicon request and stop it immediately
 
 const server = http.createServer((req, res) => {
-  console.log(req);
+  // control for favicon
+
+  /*   if (req.url === "/favicon.ico") {
+    res.writeHead(204, { "Content-Type": "image/x-icon" });
+    res.end();
+    console.log("favicon requested");
+    return;
+  }
+ */
+
+  const url = req.url;
+  if (url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Enter message</title></head>");
+    res.write(
+      "<body><form action='/message' method='POST'><input type='text' name='message'><button type='submit'>Send</button></form></body>",
+    );
+    res.write("</html>");
+    return res.end();
+  }
+
+  if (url === "/message" && req.method === "POST") {
+    const body = [];
+    req.on("data", (chunk) => {
+      body.push(chunk);
+    });
+    req.on("end", () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split("=")[1];
+      fs.writeFileSync("message.txt", message);
+    });
+    fs.writeFileSync("message.txt", "DUMMY");
+    res.statusCode = 302;
+    res.setHeader("Location", "/");
+    return res.end();
+  }
+
+  console.log(req.url, req.method, req.headers);
+  res.setHeader("Content-Type", "text/html");
+  res.write("<html>");
+  res.write("<head><title>My First Page</title></head>");
+  res.write("<body><h1>Hello from my Node.js Server!</h1></body>");
+  res.write("</html>");
+  res.end();
 });
 
 server.listen(3000);
